d0126c5202a6d96af8cbd435fc4d4cbb55bfabfd
Rafiul Islam <rafi.cse.ru@gmail.com>
2025-10-04 21:56:42 +0600

    chore: Initialize full-stack app structure (client + server)

---
+++ server/src/test/java/com/taskbuddy/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
