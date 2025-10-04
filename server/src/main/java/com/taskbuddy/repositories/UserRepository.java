f531165b29a7de41d72e5b3492dda56ea17b1139
Rafiul Islam <rafi.cse.ru@gmail.com>
2025-10-04 22:37:03 +0600

    feat: Refactor Task and User entities to establish a relationship and simplify column definitions

---
+++ server/src/main/java/com/taskbuddy/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.taskbuddy.repositories;
+
+import com.taskbuddy.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
